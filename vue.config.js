cc4202850154c956b68fcf329e69e992446af778
Neil Yang <yangnei@amazon.com>
2021-09-04 15:46:21 -0400

    docker and nginx

--- vue.config.js
+++ vue.config.js
@@ -106,7 +106,7 @@ const vueConfig = {
     // If you want to turn on the proxy, please remove the mockjs /src/main.jsL11
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:8081',
         ws: false,
         changeOrigin: true
       }
